589423df8ea3e198b2fce772eeac3900050c532d
Karl Smith <ksmith@lanl.gov>
2019-04-16 11:39:36 -0600

    Corrected Command Packet test.

--- tests/test-ComPairCommandPacket.cxx
+++ tests/test-ComPairCommandPacket.cxx
@@ -17,6 +17,7 @@ TEST_CASE("COMMAND_PACKET") {
 		};
 
 	SECTION("Type Test") {
+		//Loop over all pairs of type defined above and check that we get the correct type.
 		for (auto pair : types) {
 			data.at(0) = pair.second;
 			p.ParseData(data);
@@ -27,10 +28,13 @@ TEST_CASE("COMMAND_PACKET") {
 	}
 
 	SECTION("Bad Data Test") {
+		//Data with the MSB set.
 		data.at(0) = 0x0 + 0x80;
 		REQUIRE_THROWS(p.ParseData(data));
 
+		//CZT destination with a silicon tracker bit set
 		data.at(0) = types.at(2).second + 0x1;
+		p.ParseData(data);
 		REQUIRE(p.GetDestination() == CmdPacket::Destination::CZT);
 
 		data.at(0) = 0;
