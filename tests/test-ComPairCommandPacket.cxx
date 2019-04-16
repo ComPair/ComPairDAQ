b4c11828e7c9fe033b69c7e3898be4b71b92b6ce
Karl Smith <ksmith@lanl.gov>
2019-04-16 11:08:36 -0600

    Updated command packet tests to check each possible type.

--- tests/test-ComPairCommandPacket.cxx
+++ tests/test-ComPairCommandPacket.cxx
@@ -5,11 +5,34 @@
 typedef ComPairCommandPacket CmdPacket;
 
 TEST_CASE("COMMAND_PACKET") {
-	std::vector< uint8_t > data = {0x10, 0x0, 0x4, 0x0};
-	SECTION("Test the type") {
-		CmdPacket p;
-		p.ParseData(data);
+	CmdPacket p;
+	std::vector< uint8_t > data = {0x0, 0x0, 0x4, 0x0};
 
+	std::vector< std::pair< CmdPacket::Destination, uint8_t > > types = {
+			std::make_pair(CmdPacket::Destination::ACD, 0x4),
+			std::make_pair(CmdPacket::Destination::SI_TR, 0x8),
+			std::make_pair(CmdPacket::Destination::CZT, 0x10),
+			std::make_pair(CmdPacket::Destination::CSI, 0x20),
+			std::make_pair(CmdPacket::Destination::TRIG, 0x40),
+		};
+
+	SECTION("Type Test") {
+		for (auto pair : types) {
+			data.at(0) = pair.second;
+			p.ParseData(data);
+			REQUIRE(p.GetDestination() == pair.first);
+		}
+		//Reset first word
+		data.at(0) = 0x0;
+	}
+
+	SECTION("Bad Data Test") {
+		data.at(0) = 0x0 + 0x80;
+		REQUIRE_THROWS(p.ParseData(data));
+
+		data.at(0) = types.at(2).second + 0x1;
 		REQUIRE(p.GetDestination() == CmdPacket::Destination::CZT);
+
+		data.at(0) = 0;
 	}
 }
